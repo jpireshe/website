35032717c13aa664344f998752d63efa49b559b5
joao <Joao.Henares@btgpactual.com>
2023-10-16 01:18:21 -0300

    testing changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+function App(): React.ReactElement {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>Toin e Jonas sendo refeito.</p>
         <p> pinpin </p>
         <a
