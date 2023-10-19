f9dfd6dfe0bcad516cd5a311d0f3f2f0af7e7b0e
joao <Joao.Henares@btgpactual.com>
2023-10-19 02:54:08 -0300

    creating navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
 // filename -App.js
 
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 import Home from "./pages/home";
 import About from "./pages/about";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-      </Routes>
-
-      <Routes>
         <Route path="/about" element={<About />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
